627927df09d0ac9d15b33e7d12ecbb1d583eb1ba
Oliverg4e <56056717+Oliverg4e@users.noreply.github.com>
2021-05-22 23:10:29 -0600

    Add files via upload

---
+++ cuadros.js
@@ -0,0 +1,21 @@
+class Cuadro{
+
+	constructor(x,y,tam){
+		this.x=x;
+		this.y=y;
+		this.tam=tam;
+		
+		
+	}
+
+	display(){
+	
+		square(this.x,this.y,this.tam);
+	}
+
+	
+
+
+
+
+}
--- sketch.js
+++ sketch.js
@@ -1,25 +1,73 @@
 
-
-
+var c1;
+var c2;
+var c3;
 function setup(){
 	createCanvas(windowWidth,windowHeight);
+	stroke('red');
+	strokeWeight(4);
+	c1 = new Cuadro(750,500,400);
+	c2 = new Cuadro(c1.x+555,500,400);
+	c3 = new Cuadro(c2.x+555,500,400);
 	
 }
 
 function draw(){
-	stroke('red');
-	bh(width/2,0,width/2,height);
-	bh(0,height/2,width,height/2);
-	bh(0,0,width,height);
-	bh(0,height,width,0);
-	/*
+	
+	
+	//bh(0,height/2,width,height/2); horizontal
+	//bh(0,0,width,height);
+	//bh(0,height,width,0);
+	
+	c1.display();
+	c2.display();
+	c3.display();
 
-	stroke('blue');
-	ecuPP(width/2,0,500,500);
-	ecuPP(0,height/2,width,height/2);
-	ecuPP(0,0,width,height);
-	ecuPP(0,height,0,width,0);
-	*/
+
+	textSize(32);
+	text('Bresenham', c1.x+100, c1.y-100);
+
+	bh(c1.x,c1.y+c1.tam,c1.x+c1.tam,c1.y);
+
+	bh(c1.x,c1.y,c1.x+c1.tam,c1.y+c1.tam);
+	//ecuPP(width/2,0,width/2,height); vertical
+	bh(c1.x,c1.y+c1.tam/2,c1.x+c1.tam,c1.y+c1.tam/2)
+
+	bh(c1.x+c1.tam/2,c1.y,c1.x+c1.tam/2,c1.y+c1.tam);
+	
+
+
+
+
+	textSize(32);
+	text('DDA', c2.x+150, c2.y-100);
+
+	//dda(c2.x,c2.y+c2.tam,c2.x+c2.tam,c2.y); error diagonal
+
+	dda(c2.x,c2.y,c2.x+c2.tam,c2.y+c2.tam);
+
+	dda(c2.x,c2.y+c2.tam/2,c2.x+c2.tam,c2.y+c2.tam/2)
+
+	dda(c2.x+c2.tam/2,c2.y,c2.x+c2.tam/2,c2.y+c2.tam);
+
+
+
+	textSize(32);
+	text('Punto pendiente', c3.x+100, c3.y-100);
+
+	ecuPP(c3.x,c3.y+c3.tam,c3.x+c3.tam,c3.y);
+
+	ecuPP(c3.x,c3.y,c3.x+c3.tam,c3.y+c3.tam);
+	
+	ecuPP(c3.x,c3.y+c3.tam/2,c3.x+c3.tam,c3.y+c3.tam/2)
+
+	ecuPP(600,600,60,60); //error
+	
+
+
+	
+
+	noLoop();
 }
 
 function ecuPP(x1, y1, x2, y2){
@@ -53,83 +101,51 @@ function ecuPP(x1, y1, x2, y2){
 	}
 }
 
-function dda(x1, y1, x2, y2){
+function dda(x1, y1, x2, y2) {
 	let x = x1;
-	let y = y1;
-	
-
-	const dx = x2-x1;
-	const dy = y2-y1;
-	const m = dy/dx;
-	//pendiente +
-	
-
-
-	if(m > 0){
-		if(m<=1){
-			dx==1;
-			while(x!=x2){
-				point(x,y);
-				x++;
-				y=y+m
-			}	
-		} // pendiente -
-	} else{
-		while(x>x2){
-			point(x,y);
-			x--;
-			y=y-m;
-		}
-	}
-
+  	let y = y1;
+  	const dx = x2 - x1;
+ 	const dy = y2 - y1;
+  	const m = dy / dx;
+  	const s = m >= 0 ? 1 : -1;
+  //pendiente +
+  	if (m <= s) {
+    	while (x != x2) {
+      	point(x,y);
+      	x += s;
+      	y = y + m * s;
+    	}
+  	} else { //pendiente -
+    	while (y != y2) {
+      	point(x,y);
+      	y += s;
+      	x = x + s / m;
+    	}
+  	}
+  
 }
 
-function bh(x1,y1,x2,y2){
-	let x = x1;
-	let y = y1;
-	const dx = x2-x1;
-	const dy = y2-y1;
-	const dx2= dx*2;
-	const dy2= dy*2;
-	const m = dy/dx;
-	var p;
-	let i=0;
 
-	if( m<=1){
-		p=dy2-dx;
-		while(i!=dx){
-			if(p<0){
-				x=x+1
-				point(x,y);
-				p=p+dy2;
-				i++;	
-			}else{
-				x=x+1;
-				y=y+1;
-				point(x,y);
-				p=p+dy2;
-				i++
-			}
-		}
-	}else if(m>=1){
-		p=dx2-dy;
-		while(i!=dy){
-			if(p<0){
-				y=y+1;
-				point(x,y);
-				p=p+dx2;
-				i++;
+function bh(x1, y1, x2, y2){
+	var dx = Math.abs(x2 - x1);
+    var sx = x1 < x2 ? 1 : -1;
+    var dy = Math.abs(y2 - y1);
+    var sy = y1 < y2 ? 1 : -1;
+    var err = dx > dy ? dx : -dy;
 
-			}else{
-				x=x+1;
-				y=y+1;
-				point(x,y);
-				p=p+(dx2-dy2);
-				i++;
-			}
-		}
-	}	
-	noLoop();
+	  while (x1 != x2 || y1 != y2) {
+	      point(x1, y1);
+	      var e2 = err;
+	      if (e2 > -dx * 2) {
+	          err -= dy;
+	          x1 += sx;
+	     }
+	     if (e2 < dy * 2) {
+	         err += dx;
+	         y1 += sy;
+	    }
+	  }
+
 }
 
 
